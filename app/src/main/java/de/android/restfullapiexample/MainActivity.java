ef2c0e48aa047c63cc50dc43e7ff109a06e93698
Ayrat <ayrat1@mail.ru>
2016-08-20 00:30:18 +0300

    add realization with async task

--- app/src/main/java/de/android/restfullapiexample/MainActivity.java
+++ app/src/main/java/de/android/restfullapiexample/MainActivity.java
@@ -1,15 +1,19 @@
 package de.android.restfullapiexample;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
-
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,19 +27,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
     }
-
     public void chuckNorrisClick(View view) {
-        Ion.with(this)
-                .load("http://api.icndb.com/jokes/random")
-                .asString()
-                .setCallback(new FutureCallback<String>() {
-                    @Override
-                    public void onCompleted(Exception e, String data) {
-                        setChuckNorrisJokeToView(data);
-                    }
-                });
+        /*
+        with Ion library
+        */
+//        Ion.with(this)
+//                .load("http://api.icndb.com/jokes/random")
+//                .asString()
+//                .setCallback(new FutureCallback<String>() {
+//                    @Override
+//                    public void onCompleted(Exception e, String data) {
+//                        setChuckNorrisJokeToView(data);
+//                    }
+//                });
+
+        /*
+        with async task
+         */
+        new ChuckNorrisAsyncTask().execute("http://api.icndb.com/jokes/random");
     }
 
+        /*
+        with Ion library
+        */
     private void setChuckNorrisJokeToView(String data) {
         try {
             JSONObject response = new JSONObject(data);
@@ -43,6 +57,52 @@ public class MainActivity extends AppCompatActivity {
             chuckNorrisTextView.setText(joke);
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+    /*
+    with async task
+     */
+    private class ChuckNorrisAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String text = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setConnectTimeout(30000);
+                conn.setReadTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.connect();
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream input = conn.getInputStream();
+                    StringBuilder sb = new StringBuilder();
+                    while (true) {
+                        int ch = input.read();
+                        if (ch == - 1) break;
+                        sb.append((char) ch);
+                    }
+                    text = sb.toString();
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+            try {
+                JSONObject response = new JSONObject(data);
+                String joke = response.getJSONObject("value").getString("joke");
+                chuckNorrisTextView.setText(joke);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
