285bb2a6209a11bb78a300a6080b1db1874813c6
Ayrat <ayrat1@mail.ru>
2016-08-22 00:49:56 +0300

    AddCatsImage activity complete 01.12.21

--- app/src/main/java/de/android/restfullapiexample/AddCatsImagesActivity.java
+++ app/src/main/java/de/android/restfullapiexample/AddCatsImagesActivity.java
@@ -2,11 +2,16 @@ package de.android.restfullapiexample;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import butterknife.BindView;
@@ -16,12 +21,14 @@ import stanford.androidlib.xml.XML;
 
 public class AddCatsImagesActivity extends SimpleActivity {
     @BindView(R.id.grid) GridLayout gridLayout;
+    private int photoSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cats_images);
         ButterKnife.bind(this);
+        photoSize = (getWindowManager().getDefaultDisplay().getWidth() - 32) / 2;
     }
 
     public void onCatsClick(View view) {
@@ -37,7 +44,29 @@ public class AddCatsImagesActivity extends SimpleActivity {
     }
 
     private void processCatData(String data) {
-        JSONObject json = XML.toJSONObject(data);
+        try {
+            gridLayout.removeAllViews();
+            JSONObject json = XML.toJSONObject(data);
+            JSONArray array = json.getJSONObject("response")
+                    .getJSONObject("data")
+                    .getJSONObject("images")
+                    .getJSONArray("image");
+            for (int i = 0; i < array.length(); i++) {
+                String url = array.getJSONObject(i).getString("url");
+                ImageView imageView = new ImageView(this);
+                ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                imageView.setLayoutParams(params);
+                Picasso.with(this)
+                        .load(url)
+                        .resize(photoSize, photoSize)
+                        .into(imageView);
+                gridLayout.addView(imageView);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 }
