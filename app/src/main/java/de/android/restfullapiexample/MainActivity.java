a8ba35acb3f8fff67c11884208437cb07d6aade8
Ayrat <ayrat1@mail.ru>
2016-08-28 00:15:24 +0300

    some refactoring

--- app/src/main/java/de/android/restfullapiexample/MainActivity.java
+++ app/src/main/java/de/android/restfullapiexample/MainActivity.java
@@ -1,5 +1,9 @@
 package de.android.restfullapiexample;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -31,6 +35,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG ="TAG";
+    private static final String ACTION_DONE = "ACTION_DONE";
     @BindView(R.id.chuckNorrisTextView) TextView chuckNorrisTextView;
 
     @Override
@@ -38,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_DONE);
+        registerReceiver(new MyBroadcastReceiver(), filter);
     }
-    public void chuckNorrisClick(View view) {
+    public void onION(View view) {
         /*
-        with Ion library
-        */
+         *with Ion library
+         */
         Ion.with(this)
                 .load("http://api.icndb.com/jokes/random")
                 .asString()
@@ -54,18 +63,18 @@ public class MainActivity extends AppCompatActivity {
                 });
 
         /*
-        with async task
+         *with async task
          */
 //        new ChuckNorrisAsyncTask().execute("http://api.icndb.com/jokes/random");
     }
 
         /*
-        with Ion library
-        */
+         *with Ion library
+         */
     private void setChuckNorrisJokeToView(String data) {
         setResponseToView(data);
         /*
-        with GSON library
+         *with GSON library
          */
 //        setGsonResponseToView(data);
     }
@@ -113,8 +122,60 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void onBroadcast(View view) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String text = null;
+                try {
+                    URL url = new URL("http://api.icndb.com/jokes/random");
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setConnectTimeout(30000);
+                    conn.setReadTimeout(10000);
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    int responseCode = conn.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        InputStream input = conn.getInputStream();
+                        StringBuilder sb = new StringBuilder();
+                        while (true) {
+                            int ch = input.read();
+                            if (ch == - 1) break;
+                            sb.append((char) ch);
+                        }
+                        text = sb.toString();
+                        Intent done = new Intent();
+                        done.setAction(ACTION_DONE);
+                        done.putExtra("text", text);
+                        sendBroadcast(done);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void onION_plus_GSON(View view) {
+        Ion.with(this)
+                .load("http://api.icndb.com/jokes/random")
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String data) {
+                        setGsonResponseToView(data);
+                    }
+                });
+    }
+
+    public void onAsyncTask(View view) {
+    }
+
     /*
-    with async task
+     * with async task
      */
     private class ChuckNorrisAsyncTask extends AsyncTask<String, Void, String> {
         @Override
@@ -153,5 +214,13 @@ public class MainActivity extends AppCompatActivity {
             setResponseToView(data);
         }
 
+    }
+    private class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String data = intent.getStringExtra("text");
+            setResponseToView(data);
+        }
     }
 }
